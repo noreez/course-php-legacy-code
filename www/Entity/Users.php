fadb73507b247f46faa54683b5f1ad52db4ce464
Noreez Ul Hassan Shah <noreez.hassan@gmail.com>
2019-05-10 15:27:39 +0200

    Update - Fix

---
+++ www/Entity/Users.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Entity;
+
+use Model\UserInterface;
+use Model\UsersRepository;
+
+class Users implements UserInterface
+{
+	public $id = null;
+	public $email;
+	public $pwd;
+	public $role = 1;
+	public $status = 0;
+	private $userRepository;
+	public $identity;
+	public function __construct(UsersRepository $usersRepository, Identity $identity)
+	{
+		$this->userRepository=$usersRepository;
+		$this->identity=$identity;
+	}
+	public function setId(int $id)
+	{
+		$this->id = $id;
+		$this->userRepository->getOneBy(['id' => $id], true);
+	}
+	public function setEmail(string $email)
+	{
+		$this->email = strtolower(trim($email));
+	}
+	public function setPwd(string $pwd)
+	{
+		$this->pwd = password_hash($pwd, PASSWORD_DEFAULT);
+	}
+	public function setRole(string $role)
+	{
+		$this->role = $role;
+	}
+	public function setStatus(string $status)
+	{
+		$this->status = $status;
+	}
+	public function getId(): int
+	{
+		return $this->id;
+	}
+	public function getEmail(): string
+	{
+		return $this->email;
+	}
+	public function getPwd(): string
+	{
+		return $this->pwd;
+	}
+	public function getRole(): int
+	{
+		return $this->role;
+	}
+	public function getStatus(): int
+	{
+		return $this->status;
+	}
+}
